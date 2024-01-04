ba127e126eb25f46cb0961533caba10303d45829
Cody <codycabreraa@gmail.com>
2024-01-04 17:21:57 -0500

    Cambios

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package problemas;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -15,7 +15,30 @@ public class Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("Bienvenidos\nDigite 1 para calcular el area"
+                + "de un cuadrado, digite 2 para calcular el area de un "
+                + "triangulo y 3 para obtener el area de un rectangulo");
+        opcion = entrada.nextInt();
+        if (opcion == 1){
+            obtenerCuadrado();
+        } else if (opcion == 2) {
+            obtenerTriangulo();
+        } else if (opcion == 3){
+            obtenerRectangulo();
+        } else {
+            System.out.println("Error");
+        }
+    }
+    public static void obtenerCuadrado(){
+        Scanner entrada = new Scanner(System.in);
+        double area;
+        double lado;
+        System.out.println("Ingrese el tamaño del cuadrado");
+        lado = entrada.nextDouble();
+        area = lado * lado * lado * lado;
+        System.out.println("El area es: " + area);
     }
     
 }
